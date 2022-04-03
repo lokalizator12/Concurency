981fd97f18204fcc8a3903e4f97cb7675058f813
lokalizator12 <83495533+lokalizator12@users.noreply.github.com>
2022-04-03 20:13:13 +0200

    Executes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,153 +1,140 @@
+import javax.swing.plaf.TableHeaderUI;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.*;
 
 public class Main {
 
-    private static final int size = 50_000_000;
-    public static final int half = size / 2;
+    public static void SinglePool(){
+        long before = System.currentTimeMillis();
+        ExecutorService executorServiceSinglePool = Executors.newSingleThreadExecutor();
+        CountDownLatch countDownLatch1 = new CountDownLatch(3);
+
+        executorServiceSinglePool.execute(new Runnable() {
+            @Override
+            public void run() {
+                long sum22 = 0;
+                for (int i = 0; i < 1_000_000; i++){
+                    if (i % 2 == 0){
+                        sum22 += i;
+                    }
+                }
+                System.out.println("1.Suma: " + sum22);
+                countDownLatch1.countDown();
+            }
+        });
+
+        executorServiceSinglePool.execute(new Runnable() {
+            @Override
+            public void run() {
+                long sum33 = 0;
+                for (int i = 0; i < 1_000_000; i++){
+                    if (i % 7 == 0){
+                        sum33 += i;
+                    }
+                }
+                System.out.println("2.Suma: " + sum33);
+                countDownLatch1.countDown();
+            }
+        });
+
+        executorServiceSinglePool.execute(new Runnable() {
+            @Override
+            public void run() {
+                List<Integer> qwestList = new ArrayList();
+                int r;
+                int count33 = 0;
+                for (int i = 0; i < 1000; i++){
+                    qwestList.add((int)(Math.random()*1000));
+                }
+                for( int x : qwestList){
+                    if (x % 2 == 0){
+                        count33++;
+                    }
+                }
+                System.out.println("3. Count - " + count33);
+                countDownLatch1.countDown();
+            }
+        });
+        executorServiceSinglePool.shutdown();
+        try {
+            countDownLatch1.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        long after = System.currentTimeMillis();
+        System.out.println("Time FixedPool: " + (after - before));
+        System.out.println("Finished Single Pool.");
+
+    }
 
     public static void main(String[] args) {
-
-       // startTimer();
-       // withoutConcurrency();
-       // with();
-
-
-        giveMeMoney();
-
-
-    }
-
-    private static void giveMeMoney(){
-        Bankomat profile1 = new Bankomat(1000);
-
-        Thread thread4 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                profile1.ifHaveMoney("kol",500);
-            }
-        });
-
-        Thread thread5 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-            profile1.ifHaveMoney("uit",300);
-            }
-        });
-
-        Thread thread6 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                profile1.ifHaveMoney("uidst",600);
-            }
-        });
-        thread4.start();
-
-        try {
-            thread4.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        try {
-            thread4.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        thread5.start();
-        thread6.start();
-
-
-    }
-
-    private static void withoutConcurrency() {
-        float[] list1 = new float[size];
-        for (int i = 0; i < list1.length; i++) {
-            list1[i] = 1f;
-        }
         long before = System.currentTimeMillis();
-        for (int i = 0; i < list1.length; i++) {
-            float f = (float) i;
-            list1[i] = (float) (list1[i] * Math.sin(0.2f + f / 5) * Math.cos(0.4f + f / 2));
-        }
-        long after = System.currentTimeMillis();
-        System.out.println("without :" + (after - before));
-    }
+        ExecutorService executorServiceFixedPool = Executors.newFixedThreadPool(3);
+        CountDownLatch countDownLatch = new CountDownLatch(3);
 
-    private static void with() {
-        float[] list1 = new float[size];
-
-        for (int i = 0; i < list1.length; i++) {
-            list1[i] = 1f;
-        }
-
-        long before1 = System.currentTimeMillis();
-        float[] list2 = new float[half];
-        float[] list3 = new float[half];
-        System.arraycopy(list1, 0, list2, 0, half);
-        System.arraycopy(list1, half, list3, 0, half);
-        Thread thread1 = new Thread(new Runnable() {
+        executorServiceFixedPool.execute(new Runnable() {
             @Override
             public void run() {
-                long before = System.currentTimeMillis();
-                for (int i = 0; i < half; i++) {
-                    float f = (float) (i + half);
-                    list2[i] = (float) (list2[i] * Math.sin(0.2f + f / 5) * Math.cos(0.4f + f / 2));
+                long sum1 = 0;
+                for (int i = 0; i < 1000000; i++){
+                    if (i % 2 == 0){
+                        sum1 += i;
+                    }
                 }
-
-                long after = System.currentTimeMillis();
-                System.out.println("arrayCopy1 :" + (after - before));
+                System.out.println("1.Suma: " + sum1);
+                countDownLatch.countDown();
             }
         });
 
-        Thread thread2 = new Thread(new Runnable() {
+        executorServiceFixedPool.execute(new Runnable() {
             @Override
             public void run() {
-                long before = System.currentTimeMillis();
-                for (int i = 0; i < list3.length; i++) {
-                    float f = (float) (i + half);
-                    list3[i] = (float) (list3[i] * Math.sin(0.2f + f / 5) * Math.cos(0.4f + 3 / 2));
+                long sum = 0;
+                for (int i = 0; i < 1000000; i++){
+                    if (i % 7 == 0){
+                        sum += i;
+                    }
                 }
-
-                long after = System.currentTimeMillis();
-                System.out.println("arrayCopy2 :" + (after - before));
+                System.out.println("2.Suma: " + sum);
+                countDownLatch.countDown();
             }
         });
 
-        thread1.start();
-        thread2.start();
+        executorServiceFixedPool.execute(new Runnable() {
+            @Override
+            public void run() {
+                List<Integer> qwestList = new ArrayList<>();
+
+                int count = 0;
+                for (int i = 0; i < 1000; i++){
+
+                    qwestList.add((int)(Math.random()*1000));
+                }
+
+                for( int x : qwestList){
+                    if (x % 2 == 0){
+                        count++;
+                    }
+                }
+                System.out.println("3. Count - " + count);
+                countDownLatch.countDown();
+            }
+        });
+        executorServiceFixedPool.shutdown();
         try {
-            thread1.join();
-            thread2.join();
+            countDownLatch.await();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.arraycopy(list2, 0, list1, 0, list2.length);
-        System.arraycopy(list3, 0, list1, half, list3.length);
         long after = System.currentTimeMillis();
-        System.out.println("time with :" + (after - before1));
+        System.out.println("Finished Fixed Pool.");
+        System.out.println("Time FixedPool: " + (after - before));
+       SinglePool();
+
     }
-
-    public static void startTimer() {
-
-        Thread timer = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int second4 = 0;
-                while (true){
-                    System.out.println(second4++);
-                }
-
-            }
-        });
-        timer.start();
-        timer.setDaemon(true);
-    }
-
-
-
 
 }
 
